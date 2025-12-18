582cc5e8d2baaa1a3f4a50d32ef0a08b8a76c71f
bakhtawarshigrie <Bakhtawaralishigri@gmail.com>
2025-12-19 03:18:10 +0500

    social links UI line fix

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -200,7 +200,7 @@ export default function Footer() {
               <p className="text-sm text-gray-600 mb-6 leading-relaxed">
                 Get the latest updates on promotions, new products, much more.
               </p>
-              <div className="flex flex-wrap border-t border-l border-gray-200">
+              <div className="flex flex-wrap border-t border-l border-gray-200 max-w-[145px]">
                 {/* Facebook */}
                 <a href="https://www.facebook.com/nayabwatch/" target="_blank" className="w-12 h-12 flex items-center justify-center border-r border-b border-gray-200 bg-[#f9f9f9] text-black hover:bg-black hover:text-white transition-all duration-300">
                   <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" viewBox="0 0 16 16"><path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951z"/></svg>
